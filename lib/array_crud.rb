0009af6f9a5d51a355885ed7756fe1c89384a00c
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-07-16 17:42:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,43 +3,42 @@ def create_an_empty_array
 end
 
 def create_an_array
-    ["books", "computer", "pens", "pencil"]
+  my_array =  ["books", "computer", "pens", "pencil"]
 end
 def add_element_to_end_of_array(array, element)
-    ["books", "computer", "pens", "pencil"]
-    add_element_to_end_of_array << "notebook"
+  my_array = ["books", "computer", "pens", "pencil"]
+    my_array << "notebook"
 end
 
 def add_element_to_start_of_array(array, element)
-    ["books", "computer", "pens", "pencil"]
-     add_element_to_start_of_array.unshift "notebook"
+    my_array = ["books", "computer", "pens", "pencil"]
+    my_array.unshift "notebook"
 end
 
 def remove_element_from_end_of_array(array)
-    ["books", "computer", "pens", "pencil"]
-     remove_element_from_end_of_array.pop
+  my_array = ["books", "computer", "pens", "pencil"]
+    my_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-     ["books", "computer", "pens", "pencil"]
-      remove_element_from_start_of_array.shift
+  my_array = ["books", "computer", "pens", "pencil"]
+      my_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-    ["books", "computer", "pens", "pencil"]
-     retrieve_element_from_index [3]
+  my_array =  ["books", "computer", "pens", "pencil"]
+    my_array [3]
 end
 
 def retrieve_first_element_from_array(array)
-    ["books", "computer", "pens", "pencil"]
+  my_arry = ["books", "computer", "pens", "pencil"]
     retrieve_first_element_from_array [0]
 end
 
 def retrieve_last_element_from_array(array)
-    retrieve_last_element_from_array [-1]
+    my_array [-1]
 end
 def update_element_from_index(array, index_number, element)
-    ["books", "computer", "pens", "pencil"]
-    update_element_from_index [1]
-    update_element_from_index [1] "pencil"
+    my_array = ["books", "computer", "pens", "pencil"]
+    my_array [1] = "notebook"
 end
