30b28c6340d1bc180dbf5468f17e73c301d84e2e
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-07-16 17:13:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,47 @@
 def create_an_empty_array
-  
+    create_an_empty_array = []
 end
-
 def create_an_array
-  
-end
+  create_an_array = ["books", "computer", "pens", "pencil"]
 
 def add_element_to_end_of_array(array, element)
-  
+
+  add_element_to_end_of_array = ["books", "computer", "pens", "pencil"]
+
+  add_element_to_end_of_array << "notebook"
 end
 
 def add_element_to_start_of_array(array, element)
-  
-end
 
+    add_element_to_start_of_array = ["books", "computer", "pens", "pencil"]
+
+    add_element_to_start_of_array.unshift "notebook"
+end
 def remove_element_from_end_of_array(array)
-  
-end
+    remove_element_from_end_of_array = ["books", "computer", "pens", "pencil"]
 
+    remove_element_from_end_of_array.pop
+end
 def remove_element_from_start_of_array(array)
-  
-end
+    remove_element_from_start_of_array = ["books", "computer", "pens", "pencil"]
 
+    remove_element_from_start_of_array.shift
+end
 def retrieve_element_from_index(array, index_number)
-  
-end
+    retrieve_element_from_index = ["books", "computer", "pens", "pencil"]
 
+    retrieve_element_from_index [3]
+end
 def retrieve_first_element_from_array(array)
-  
+    retrieve_first_element_from_array = ["books", "computer", "pens", "pencil"]
+    retrieve_first_element_from_array [0]
 end
-
 def retrieve_last_element_from_array(array)
-  
+    retrieve_last_element_from_array [-1]
 end
-
 def update_element_from_index(array, index_number, element)
-
+    update_element_from_index = ["books", "computer", "pens", "pencil"]
+    update_element_from_index [1]
+    update_element_from_index [1] = "pencil"
 end
+end 
